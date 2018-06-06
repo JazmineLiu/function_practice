008b866a3a72bbc5160bd61136f4aa4c1bef5913
your_name <vrilgil158429@outlook.com>
2018-06-06 21:16:50 +0800

    hi

---
+++ function_homework.py
@@ -0,0 +1,36 @@
+def doo(n):
+	num = (6 * n) - 4
+	return num 
+
+
+print(doo(4)) 
+print(doo(5)) 
+print(doo(6)) 
+print(doo(7)) 
+
+
+
+def jaz(b):
+	num = 1 / (3 ** b - 1)
+	return num
+
+
+print(jaz(1))
+print(jaz(2))
+print(jaz(3))
+print(jaz(4))
+print(jaz(5))
+
+
+
+
+def ahhhhh(c):
+	num = (-1 ** c) * (3)
+	return num
+
+
+print(ahhhhh(1))
+print(ahhhhh(2))
+print(ahhhhh(3))
+print(ahhhhh(4))
+print(ahhhhh(5))
